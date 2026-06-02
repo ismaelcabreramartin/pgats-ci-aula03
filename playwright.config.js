ceedb332e3b4b74711fb88e2f42c82588c79f743
Ismael <ismael.cabrera.martin@gmail.com>
2026-06-01 23:27:31 -0300

    adicionando relatorio na pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: false }], ['junit', { outputFile: `results.xml` }]],
+  reporter: [
+  ['html'],                                      // relatório HTML local
+  ['playwright-ctrf-json-reporter', {}]          // gera ctrf-report.json
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
